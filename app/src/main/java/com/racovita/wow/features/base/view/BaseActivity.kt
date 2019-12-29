e063673e60271add671000fe34d0303c28b09868
Vasile Doe <vasile.doe1992@gmail.com>
2019-12-29 10:38:33 +0200

    - add Splash screen;

---
+++ app/src/main/java/com/racovita/wow/features/base/view/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.racovita.wow.features.base.view
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Common functional for all Activities
+ */
+@SuppressLint("Registered")
+open class BaseActivity : AppCompatActivity()
